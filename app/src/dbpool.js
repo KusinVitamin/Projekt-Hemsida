c746064f24fb3ac8d4c90554619cdf8b2a8e742b
Rickos99 <rickos99@hotmail.com>
2021-02-25 12:23:45 +0100

    Tagit bort kommentar

--- app/src/dbpool.js
+++ app/src/dbpool.js
@@ -3,10 +3,6 @@ var mysql = require("mysql");
 /** @type {mysql.Pool} */
 var pool = mysql.createPool({
     connectionLimit: 10,
-    //host: "127.0.0.1",
-    // user : 'sa',
-    // password : 'D0020E-project-AL',
-    // database: "projectal",
     host: process.env.DB_host,
     user: process.env.DB_user,
     password: process.env.DB_password,
